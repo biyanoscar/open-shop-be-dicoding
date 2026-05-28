9459461adb49d365b5d14baf69d61a3da9e4be2f
biyanoscar <biyanoscar@gmail.com>
2026-05-28 20:20:06 +0700

    feat: add name-based filtering to product list endpoint and reformat serializer calls

--- products/views.py
+++ products/views.py
@@ -7,23 +7,29 @@ from rest_framework import status
 from products.serializers import ProductSerializer
 from .models import Product
 from django.http import Http404
- 
+
+
 class ProductList(APIView):
     def post(self, request):
-        product = ProductSerializer(data=request.data, context={'request': request})  # Pass request context
+        product = ProductSerializer(data=request.data, context={
+                                    'request': request})  # Pass request context
         if product.is_valid(raise_exception=True):
             product.save()
             return Response(product.data, status=status.HTTP_201_CREATED)
- 
+
         return Response(product.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
         products = Product.objects.all()
+        name = request.query_params.get('name')
+        if name:
+            products = products.filter(name__icontains=name)
         serializer = ProductSerializer(products, many=True, context={
-                                    'request': request})  # Pass request context
+            'request': request})  # Pass request context
         return Response({
             "products": serializer.data
         }, status=status.HTTP_200_OK)
+
 
 class ProductDetail(APIView):
     def get_object(self, pk):
@@ -39,7 +45,8 @@ class ProductDetail(APIView):
 
     def put(self, request, pk):
         product = self.get_object(pk)
-        serializer = ProductSerializer(product, data=request.data, context={'request': request})
+        serializer = ProductSerializer(
+            product, data=request.data, context={'request': request})
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
