777d9c640fc560fdba1471ccd55437c86f7fdbf3
biyanoscar <biyanoscar@gmail.com>
2026-05-28 08:53:35 +0700

    feat: implement Product model and initial database migration with gitignore configuration: - python manage.py makemigrations - python manage.py migrate

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # open-shop-be-dicoding
+
+## Running the Application
+
+Create the DB tables first:
+```
+python manage.py migrate
+```
+Run the development web server:
+```
+python manage.py runserver
+```
+Open the URL http://localhost:8000/ to access the application.
--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    name = models.TextField()
+    sku = models.CharField(max_length=30)
+    description = models.TextField()
+    shop = models.TextField()
+    location = models.TextField()
+    price = models.IntegerField()
+    discount = models.IntegerField()
+    category = models.CharField(max_length=30)
+    stock = models.IntegerField()
+    is_available = models.BooleanField()
+    picture = models.TextField()
+    is_delete = models.BooleanField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
